99f6c0aa90e499f3d3750896605d7145bee71611
Yunus Emre Mert <yunusemremert@yandex.com>
2020-08-10 18:39:21 +0300

    v2 changed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="home">
     <h1>This is an home page</h1>
+    <Text>Merhaba Dünya!</Text>
+    <Text tag="p">Merhaba Dünya!</Text>
+    <Text tag="b" size="xsmall">Merhaba Dünya!</Text>
+    <IconHome width="60" height="60" />
   </div>
 </template>
+
+<script>
+import Text from '@/components/Text'
+import IconHome from '../icons/home.svg'
+
+export default {
+  name: 'Home',
+  components: {
+    Text,
+    IconHome
+  }
+}
+</script>
 
 <style scoped>
 h1 {
