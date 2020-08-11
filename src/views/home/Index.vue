569c8cd5685c89c8160cb97889603e1d20b39a43
Yunus Emre Mert <yunusemremert@yandex.com>
2020-08-11 15:37:19 +0300

    Home
    Layout ekranı düzeltme

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,25 +1,32 @@
 <template>
   <div class="home">
-    <h1>This is an home page</h1>
-    <TextComp>Merhaba Dünya!</TextComp>
-    <TextComp tag="p">Merhaba Dünya!</TextComp>
-    <TextComp tag="b" size="xsmall">Merhaba Dünya!</TextComp>
-    <IconHome width="60" height="60" />
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-      asperiores autem dicta doloremque dolores ea facilis incidunt, inventore
-      ipsam iusto magni nemo neque provident quaerat quam sequi ut vel vitae?
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-      assumenda autem, beatae consectetur culpa inventore ipsum molestiae nam
-      numquam officia officiis optio quidem, sint! A alias blanditiis eos vero
-      voluptatum? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      Atque, beatae commodi dolore dolorem earum eveniet impedit in laboriosam
-      magnam minus nam natus obcaecati possimus repellendus suscipit tenetur,
-      vel voluptate voluptatem. Lorem ipsum dolor sit amet, consectetur
-      adipisicing elit. Consequatur dolore error illo itaque mollitia neque
-      perspiciatis quasi? Delectus, eos ex, ipsa ipsam magni modi molestias
-      perferendis possimus praesentium quasi reiciendis.
-    </p>
+    <div class="timeline">
+      <h1>This is an home page</h1>
+      <TextComp>Merhaba Dünya!</TextComp>
+      <TextComp tag="p">Merhaba Dünya!</TextComp>
+      <TextComp tag="b" size="xsmall">Merhaba Dünya!</TextComp>
+      <IconHome width="60" height="60" />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+        asperiores autem dicta doloremque dolores ea facilis incidunt, inventore
+        ipsam iusto magni nemo neque provident quaerat quam sequi ut vel vitae?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+        assumenda autem, beatae consectetur culpa inventore ipsum molestiae nam
+        numquam officia officiis optio quidem, sint! A alias blanditiis eos vero
+        voluptatum? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Atque, beatae commodi dolore dolorem earum eveniet impedit in laboriosam
+        magnam minus nam natus obcaecati possimus repellendus suscipit tenetur,
+        vel voluptate voluptatem. Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Consequatur dolore error illo itaque mollitia neque
+        perspiciatis quasi? Delectus, eos ex, ipsa ipsam magni modi molestias
+        perferendis possimus praesentium quasi reiciendis.
+      </p>
+    </div>
+    <div class="sidebar">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus atque
+      eius eligendi error esse harum numquam quo repellat. Adipisci amet cum
+      dolores impedit labore nemo possimus, quia saepe suscipit voluptatum.
+    </div>
   </div>
 </template>
 
@@ -37,11 +44,23 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  color: red;
+.home {
+  max-width: 605px;
+  margin-left: auto;
+  margin-right: auto;
 
   @media (--t) {
-    color: blue;
+    max-width: none;
+    display: grid;
+    grid-template-columns: 1fr 295px;
+    grid-gap: 30px;
+  }
+}
+.sidebar {
+  display: none;
+
+  @media (--t) {
+    display: block;
   }
 }
 </style>
