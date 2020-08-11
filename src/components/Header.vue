58a1b01c106c8b90daf94315ad7781b06a84fc28
Yunus Emre Mert <yunusemremert@yandex.com>
2020-08-11 15:30:52 +0300

    Header
    Header responsive tasarımı

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,124 @@
 <template>
-  <header>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/direct">Direct</router-link> |
-    <router-link to="/explore">Explore</router-link> |
-    <router-link to="/profile">Profile</router-link>
+  <header class="header">
+    <Container class="header-inner">
+      <!--- logo -->
+      <router-link to="/" class="logo">Instagram</router-link>
+
+      <!--- search -->
+      <form class="search">
+        <input type="text" placeholder="Search" />
+      </form>
+
+      <!--- navigate -->
+      <nav class="navigation">
+        <router-link to="/">
+          <IconHomeFill v-if="$route.name === 'Index'" />
+          <IconHome v-else />
+        </router-link>
+        <router-link to="/direct">
+          <IconDirectFill v-if="$route.name === 'Direct'" />
+          <IconDirect v-else />
+        </router-link>
+        <router-link to="/explore">
+          <IconExploreFill v-if="$route.name === 'Explore'" />
+          <IconExplore v-else />
+        </router-link>
+        <router-link to="/profile">Profile</router-link>
+      </nav>
+    </Container>
   </header>
 </template>
 
 <script>
+import Container from '@/components/Container'
+
+import IconHome from '@/icons/home.svg'
+import IconHomeFill from '@/icons/home-fill.svg'
+import IconDirect from '@/icons/direct.svg'
+import IconDirectFill from '@/icons/direct-fill.svg'
+import IconExplore from '@/icons/explore.svg'
+import IconExploreFill from '@/icons/explore-fill.svg'
+
 export default {
-  name: 'MainHeader'
+  name: 'MainHeader',
+  components: {
+    Container,
+    IconHome,
+    IconHomeFill,
+    IconDirect,
+    IconDirectFill,
+    IconExplore,
+    IconExploreFill
+  }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.header {
+  z-index: 90;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  background-color: white;
+  border-bottom: 1px solid rgb(var(--b6a));
+}
+.header-inner {
+  height: var(--header-height);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media (--t) {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+  }
+}
+.logo {
+  font-size: 2rem;
+  font-width: bold;
+}
+.search {
+  display: none;
+
+  @media (--t) {
+    display: block;
+    text-align: center;
+
+    input {
+      width: 215px;
+      height: 28px;
+      border: 1px solid rgb(var(--b6a));
+      border-radius: 4px;
+      background-color: rgb(var(--b3f));
+      padding-left: 20px;
+      padding-right: 20px;
+    }
+  }
+}
+.navigation {
+  z-index: 90;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: white;
+  border-top: 1px solid rgb(var(--b6a));
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  @media (--t) {
+    border: 0;
+    height: auto;
+    position: static;
+    justify-content: flex-end;
+
+    a {
+      margin-left: 20px;
+    }
+  }
+}
+</style>
--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -5,6 +5,21 @@
     <TextComp tag="p">Merhaba Dünya!</TextComp>
     <TextComp tag="b" size="xsmall">Merhaba Dünya!</TextComp>
     <IconHome width="60" height="60" />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+      asperiores autem dicta doloremque dolores ea facilis incidunt, inventore
+      ipsam iusto magni nemo neque provident quaerat quam sequi ut vel vitae?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+      assumenda autem, beatae consectetur culpa inventore ipsum molestiae nam
+      numquam officia officiis optio quidem, sint! A alias blanditiis eos vero
+      voluptatum? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Atque, beatae commodi dolore dolorem earum eveniet impedit in laboriosam
+      magnam minus nam natus obcaecati possimus repellendus suscipit tenetur,
+      vel voluptate voluptatem. Lorem ipsum dolor sit amet, consectetur
+      adipisicing elit. Consequatur dolore error illo itaque mollitia neque
+      perspiciatis quasi? Delectus, eos ex, ipsa ipsam magni modi molestias
+      perferendis possimus praesentium quasi reiciendis.
+    </p>
   </div>
 </template>
 
